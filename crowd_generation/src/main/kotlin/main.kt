ef2dbba8a492d6ee670988c92e942b3124e93101
Ekaterina <50626720+Sguhaaa@users.noreply.github.com>
2021-02-16 23:06:56 +0500

    Homework #1 17.02

---
+++ crowd_generation/src/main/kotlin/main.kt
@@ -0,0 +1,75 @@
+import kotlin.random.Random
+
+class Wizard(
+    val name: String,
+    val power: Int,
+    val phrase: String,
+    val spell: String,
+    val color: Boolean)
+
+class WizardGroupGenerator{
+    fun countGenerator() {
+
+        //Определяем верхние и нижние границы
+        var (minWizardCount, maxWizardCount) = readLine()!!.split(' ').map(String::toInt)
+
+        if (minWizardCount == maxWizardCount) {
+            maxWizardCount += 1
+        }
+        if (minWizardCount > maxWizardCount) {
+            minWizardCount = maxWizardCount.also { maxWizardCount = minWizardCount }
+        }
+
+        //Генерим волшебников
+        fun createWizard(minWizardCount: Int, maxWizardCount: Int) {
+
+            val listName = listOf("Шут", "Варрик", "Луна", "Фитц")
+
+            val startPhrase = listOf("Эй, ты! ", "Отродье. ", "Смерть глупцам. ", "Чёртов эльф! ", "Ничтожное существо. ")
+            val middlePhrase = listOf("Умри! ", "Вызываю тебя на поединок. ", "Тебе не победить. ")
+            val endPhrase = listOf("Я сражаюсь во имя Королевы.", "Мой клинок пронзит тебя.", " ")
+
+            fun generateSpell(size: Int): String {
+                val source = "A1BCDEF4G0H8IJKLM7NOPQ3RST9UVWX52YZab1cd60ef2ghij3klmn49opq5rst6uvw7xyz8"
+                return (source).map { it }.shuffled().subList(0, size).joinToString("")
+            }
+
+            fun colorWizard(power: Int): Boolean{
+                return power <= 666
+            /*
+                if (power <= 666) return true
+                else return false
+            */
+            }
+
+            println("Мы создали волшебников, вот они, сверху вниз:" + "\n")
+
+            repeat(Random.nextInt(minWizardCount, maxWizardCount)) {
+
+                val power = Random.nextInt(0, 999)
+
+                val wizard = Wizard(
+                    listName[Random.nextInt(0, listName.size)],
+                    power,
+                    (startPhrase.random() + middlePhrase.random() + endPhrase.random()),
+                    generateSpell(Random.nextInt(1, 10)),
+                    colorWizard(power)
+                )
+                println(wizard.name + " говорит: " + wizard.phrase)
+                println("Кастует случайное заклинание: " + wizard.spell)
+                println("Затем атакует с силой: " + wizard.power)
+                println("Светлый волшебник: " + wizard.color)
+                println(" ")
+            }
+        }
+
+        createWizard(minWizardCount, maxWizardCount)
+    }
+}
+
+fun main() {
+    println("Введите через пробел два числа для генерации Волшебников:")
+    val wizardGenerator = WizardGroupGenerator()
+    return wizardGenerator.countGenerator()
+}
+
